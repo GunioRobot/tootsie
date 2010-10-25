a3f55d75f7013fe12d369c8dafca52dcfc56e0e3
Alexander Staubo <alex@bengler.no>
2010-10-26 00:10:15 +0200

    Add FFmpeg progress.

--- lib/tranz/ffmpeg_adapter.rb
+++ lib/tranz/ffmpeg_adapter.rb
@@ -12,6 +12,7 @@ module Tranz
       @ffmpeg_arguments['threads'] = (options[:thread_count] || 1)
     end
     
+    # Transcode a file by taking an input file and writing an output file.
     def transcode(input_filename, output_filename, options = {})
       @output = ''
       arguments = @ffmpeg_arguments.dup
@@ -43,12 +44,29 @@ module Tranz
       }.join(' ')
       command_line << ' '
       command_line << "'#{output_filename}' 2>&1"
+
+      progress, expected_duration = @progress, nil
       IO.popen(command_line, 'r') do |output|
         output.each_line do |line|
+          if progress
+            case line
+              when /^\s*Duration: (\d+):(\d+):(\d+)\./
+                unless expected_duration
+                  hours, minutes, seconds = $1.to_i, $2.to_i, $3.to_i
+                  expected_duration = seconds + minutes * 60 + hours * 60 * 60
+                end
+              when /^frame=.* time=(\d+)\./
+                if expected_duration
+                  elapsed_time = $1.to_i
+                end
+            end
+            progress.call(elapsed_time, expected_duration) if elapsed_time
+          end
           @output << line
           @logger.info("[ffmpeg] #{line.strip}") if @logger
         end
       end
+
       status = $?
       if status.exited?       
         raise FfmpegAdapterExecutionFailed, "FFmpeg failed with exit code #{status.exitstatus}" if status.exitstatus != 0
@@ -64,7 +82,13 @@ module Tranz
     attr_accessor :ffmpeg_binary
     attr_accessor :ffmpeg_arguments
     attr_accessor :logger
+    
+    # Output captured from FFmpeg command line tool so far.
     attr_reader :output
+
+    # Progress reporter that implements +call(seconds, total_seconds)+ to record
+    # transcoding progress.
+    attr_accessor :progress
 
   end
     
