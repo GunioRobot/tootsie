4595f9e8f6e481a662644904f6f1c43085f9eac6
Alexander Staubo <alex@bengler.no>
2010-10-26 00:11:07 +0200

    Job processing failure recovery.

--- lib/tranz/job_processor.rb
+++ lib/tranz/job_processor.rb
@@ -16,55 +16,73 @@ module Tranz
       loop do
         job = @queue.pop(:wait => true)
         if job
-          @logger.info "Begin processing job: #{job.inspect}"
-          begin
-            job.notify!(:event => :started)
-            input = Input.new(job.input_url)
-            begin
-              input.get!
-              output = Output.new(job.output_url)
-              begin
-                elapsed_time = Benchmark.realtime {
-                  adapter = Tranz::FfmpegAdapter.new(
-                    :logger => @logger,
-                    :thread_count => Application.get.configuration.ffmpeg_thread_count)
-                  adapter.transcode(
-                    input.file_name,
-                    output.file_name,
-                    job.transcoding_options)
-                }                
-                output.content_type = job.transcoding_options[:content_type] if job.transcoding_options[:content_type]
-                output.put!(job.output_options || {})
-                
-                job.notify!(
-                  :event => :completed,
-                  :url => output.result_url,
-                  :time_taken => elapsed_time)
-              ensure
-                output.close
-              end
-            ensure
-              input.close
-            end
-          rescue Interrupt
-            job.notify!(:event => :failed, :reason => 'Cancelled')
-          rescue Exception => exception
-            @logger.error "Job failed with exception #{exception.class}: #{exception}"
-            if job.retries_left and job.retries_left > 0
-              job.retries_left -= 1
-              @logger.info "Pushing job back on queue to retry it"
-              @queue.push(job)
-            else
-              @logger.error "No more retries for job, marking as failed"
-              job.notify!(:event => :failed, :reason => exception.message)
-            end
-          else
-            @logger.info "Completed job #{job.inspect}"
-          end
+          handle_job(job)
         end
       end
       @logger.info "Job processor done"
     end
+    
+    private
+    
+      def handle_job(job)
+        @logger.info "Begin processing job: #{job.inspect}"
+        job.notify!(:event => :started)
+        input = Input.new(job.input_url)
+        begin
+          input.get!
+          output = Output.new(job.output_url)
+          begin
+            last_notified = nil
+            elapsed_time = Benchmark.realtime {
+              adapter = Tranz::FfmpegAdapter.new(
+                :logger => @logger,
+                :thread_count => Application.get.configuration.ffmpeg_thread_count)
+              adapter.progress = lambda { |seconds, total_seconds|
+                now = Time.now
+                if last_notified.nil? or now - last_notified > 10.seconds
+                  last_notified = now
+                  job.notify!(
+                    :event => :progress,
+                    :seconds => seconds,
+                    :total_seconds => total_seconds)
+                end
+              }
+              adapter.transcode(
+                input.file_name,
+                output.file_name,
+                job.transcoding_options)
+            }                
+            output.content_type = job.transcoding_options[:content_type] if job.transcoding_options[:content_type]
+            output.put!(job.output_options || {})
+            
+            job.notify!(
+              :event => :completed,
+              :url => output.result_url,
+              :time_taken => elapsed_time)
+          ensure
+            output.close
+          end
+        ensure
+          input.close
+        end
+      rescue Interrupt
+        job.notify!(:event => :failed, :reason => 'Cancelled')
+        raise
+      rescue Exception => exception
+        @logger.error "Job failed with exception #{exception.class}: #{exception}\n" <<
+          "#{exception.backtrace.map { |line| "#{line}\n" }.join}"
+        if job.retries_left and job.retries_left > 0
+          job.retries_left -= 1
+          @logger.info "Pushing job back on queue to retry it"
+          job.notify!(:event => :failed_will_retry, :reason => exception.message)
+          @queue.push(job)
+        else
+          @logger.error "No more retries for job, marking as failed"
+          job.notify!(:event => :failed, :reason => exception.message)
+        end
+      else
+        @logger.info "Completed job #{job.inspect}"
+      end
     
   end
   
